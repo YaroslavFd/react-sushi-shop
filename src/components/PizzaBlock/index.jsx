160f6f3aced6996682bc7f51b58f3fc6af11ef02
Yaroslav <yaroslav.web9@gmail.com>
2023-06-08 17:54:47 +0300

    added additional features with react router

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 import { addItem } from "../../redux/slices/cartSlice";
 
 const typeNames = ["тонкое", "традиционное"];
@@ -26,8 +28,10 @@ export const PizzaBlock = ({ id, imageUrl, title, types, sizes, price }) => {
   };
   return (
     <div className="pizza-block">
-      <img className="pizza-block__image" src={imageUrl} alt={title} />
-      <h4 className="pizza-block__title">{title}</h4>
+      <Link to={`/pizza/${id}`}>
+        <img className="pizza-block__image" src={imageUrl} alt={title} />
+        <h4 className="pizza-block__title">{title}</h4>
+      </Link>
       <div className="pizza-block__selector">
         <ul>
           {types.map((type, i) => {
---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+export const FullPizza = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [item, setItem] = React.useState();
+
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(
+          "https://646cc9cc7b42c06c3b2c045f.mockapi.io/items/" + id
+        );
+        setItem(data);
+      } catch (error) {
+        alert("Что-то пошлок не так :/");
+        navigate("/");
+      }
+    })();
+  }, []);
+
+  if (!item) {
+    return <h2>Загрузка...</h2>;
+  }
+
+  return (
+    <div className="container">
+      <img
+        style={{ width: 300, height: 300 }}
+        src={item.imageUrl}
+        alt={item.title}
+      />
+      <h2>{item.title}</h2>
+      <h4>{item.price} ₽</h4>
+    </div>
+  );
+};
