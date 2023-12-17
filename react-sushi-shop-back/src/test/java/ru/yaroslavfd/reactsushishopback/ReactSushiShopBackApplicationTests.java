7ce3e37befcf8dc332780df64d23a1507ac7198b
matiqof <matiqof@gmail.com>
2023-12-17 23:51:14 +0300

    #2 Создание каркаса Back-End Spring

---
+++ react-sushi-shop-back/src/test/java/ru/yaroslavfd/reactsushishopback/ReactSushiShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yaroslavfd.reactsushishopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSushiShopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
