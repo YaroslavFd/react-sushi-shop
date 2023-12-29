85e466e7167e42f9e88be4b4da393795626c6c0e
matiqof <matiqof@gmail.com>
2023-12-29 20:28:05 +0300

    #6 Настроить каркас для оптимальной работы Rest сервиса

--- react-sushi-shop-back/src/main/java/ru/yaroslavfd/reactsushishopback/ReactSushiShopBackApplication.java
+++ react-sushi-shop-back/src/main/java/ru/yaroslavfd/reactsushishopback/ReactSushiShopBackApplication.java
@@ -1,9 +1,31 @@
 package ru.yaroslavfd.reactsushishopback;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 
-@SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "",
+				version = "",
+				contact = @Contact(
+						email = "",
+						name = ""
+				)
+		)
+)
+
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		FreeMarkerAutoConfiguration.class,
+		JmsAutoConfiguration.class
+})
 public class ReactSushiShopBackApplication {
 
 	public static void main(String[] args) {
